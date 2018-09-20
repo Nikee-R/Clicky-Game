2a0791cddd4981617f5776cb91ff4e5f2de9124a
Nikee <rivera.nikee@gmail.com>
2018-09-20 13:23:43 -0400

    added screenshot

--- src/Container.js
+++ src/Container.js
@@ -3,6 +3,6 @@ import React from "react";
 const Container = props => 
     <div className={`container${props.fluid ? "-fluid" : ""}`}>
     {props.children}
-    </div>
+    </div>;
     
 export default Container;
