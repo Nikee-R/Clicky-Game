2597cc03f425e012f520723a19be31a80286cc0d
Nikee <rivera.nikee@gmail.com>
2018-09-19 23:17:48 -0400

    added more placedholder, functionality, and columns and rows for cleaner appearance

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Navbar from "./components/Navbar";
 import Title from "./components/Title";
 import Wrapper from "./components/Wrapper";
 import friends from "./friends.json";
+import Container from "./Container";
+import Column from "./Column";
+import Row from "./Row";
 import './App.css';
 
 
@@ -19,6 +22,74 @@ class App extends Component {
     friends: friends,
     unselected: friends
   }
-}
+  componentDidMount() {
+
+  }
+
+  // Handles shuffling.
+  shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i-- ) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  selectFriend = friend => {
+    const findFriend = this.state.unselected.find(item => item.friend === friend);
+
+    if (findFriend === undefined) {
+      // Lose.
+      this.setState({
+        message: "You lose!",
+        topScore: (this.state.score > this.state.topScore) ? this.state.score : this.state.topScore,
+        score: 0,
+        friends: friends,
+        unselected: friends
+      });
+    } else {
+      // Win!
+      const newFriends = this.state.unselected.filter(item => item.friend !== friend);
+
+      this.setState({
+        message: "You win!",
+        score: this.state.score + 1,
+        friends: friends,
+        unselected: newFriends
+      });
+    }
+
+    this.shuffleArray(friends);
+  };
+ 
+  render() {
+    return (
+      <Wrapper>
+        <Navbar 
+           title="Clicky Game"
+           score={this.state.score}
+           topScore={this.state.topScore}
+           />
+          <Title>
+            Try to click on each image, but don't hit any duplicates!
+          </Title>
+
+          <Container>
+            <Row>
+              {this.state.friends.map(friend => (
+                <Column size="md-3 sm-6">
+                  <FriendCard
+                    friend={friends.friend}
+                    image={friends.image}
+                    selectFriend={this.selectFriend}
+                    score={this.state.score}
+                    />
+                </Column>
+              ))};
+            </Row>
+          </Container>
+      </Wrapper>
+    );
+  };
+};
 
 export default App;
